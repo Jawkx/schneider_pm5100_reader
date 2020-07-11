0c0f58d42a7590ea48b641c6655e153e40bc6fb5
Jawkaixuan <j_kaixuan@live.com>
2020-07-11 16:05:19 +0800

    Added multiple rotation

--- meter_recognize_oop.py
+++ meter_recognize_oop.py
@@ -5,19 +5,48 @@ import matplotlib.pyplot as plt
 
 dtype1_lbl = ["V avg","I avg","P tot","E del"]
 
-#loads template
-#type1 
 temptype1 = []
-
 for x in range(0,10):
     temp = cv2.imread("templates/"+ str(x) +".png" ,0)
     temptype1.append(temp)
 
-def findDevice(img,dtype):
-    if dtype == 0:
-        threshold = 40
-        out = np.float32([[0,300],[0,0],[300,0],[300,300]])
+type1 = {
+	"deviceThresh" : 40,
+	"screenThresh" : 80,
+	"outpar" : np.float32([[0,300],[0,0],[300,0],[300,300]]) ,
+	"outsize" : (300,300),
 
+	"ratiobase" : 25,
+    "morphsize" : (5,1),
+    "dilation_no" : 3,
+    "maxcount" : 4,
+    "temps" : temptype1,
+
+    "digitmorphsize" : (1,10),
+    "digit_dilation_no" : 2,
+    "Label" : ["V avg","I avg","P tot","E del"],
+    "decpoint" : [3,3,3,1],
+    "dighighratio" : 0.85
+}
+
+
+
+def rotateimg(img):
+	rotated = img
+	while True:
+		cv2.imshow("Rotate?",rotated)
+		k = cv2.waitKey(0)
+		cv2.destroyAllWindows()
+		if k == 114 or k ==82:
+			rotated = cv2.rotate(rotated, cv2.cv2.ROTATE_90_CLOCKWISE) 
+		elif k == 32:
+			break
+
+	return rotated
+
+def findDevice(img,dtype):
+    threshold = dtype["deviceThresh"]
+    out = dtype["outpar"]
     im_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(im_grey, threshold, 255, cv2.THRESH_BINARY_INV)
 
@@ -27,12 +56,12 @@ def findDevice(img,dtype):
     box = cv2.boxPoints(rect).astype("int")
     
     M = cv2.getPerspectiveTransform(np.float32(box),out)
-    return cv2.warpPerspective(im_grey,M,(300,300))
+    return cv2.warpPerspective(im_grey,M,dtype["outsize"])
 
 def findScreen(device,dtype):
-    if dtype == 0:
-        threshold = 80
-        out = np.float32([[0,300],[0,0],[300,0],[300,300]])
+
+    threshold = dtype["screenThresh"]
+    out = dtype["outpar"]
         
     ret,thresh = cv2.threshold(device, threshold, 255, cv2.THRESH_BINARY)
     screencontours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -41,23 +70,20 @@ def findScreen(device,dtype):
         rect = cv2.minAreaRect(screen)
         box2 = cv2.boxPoints(rect).astype("int")
         M = cv2.getPerspectiveTransform(np.float32(box2),out)
-        screen = cv2.warpPerspective(device,M,(300,300))
+        screen = cv2.warpPerspective(device,M,dtype["outsize"])
     else:
      print("ERROR CAN'T FIND SCREEN")
      
-    cv2.imshow("press spacebar to rotate, any to continue",screen)
-    k = cv2.waitKey(0)
-    if k == 32:
-        screen = cv2.rotate(screen, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE) 
-    cv2.destroyAllWindows()
+    screen = rotateimg(screen)
+
     return screen
 
 def extractTextImg(screen,dtype):
-    if dtype == 0:
-        ratiobase = 25
-        morphsize = (5,1)
-        dilation_no = 3
-        maxcount = 4
+
+    ratiobase = dtype["ratiobase"]
+    morphsize = dtype["morphsize"]
+    dilation_no = dtype["dilation_no"]
+    maxcount = dtype["maxcount"]
     
     croppedlist = []
     threshold = 255
@@ -108,8 +134,8 @@ def extractDigit(numblock,dtype):
     cv2.imshow("show",numblock)
     hblock,wblock = numblock.shape
     
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,10)) 
-    dilation = cv2.dilate(numblock, rect_kernel, iterations = 2) 
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, dtype["digitmorphsize"]) 
+    dilation = cv2.dilate(numblock, rect_kernel, iterations = dtype["digit_dilation_no"]) 
     cnts = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     extractedcount = 0
@@ -117,7 +143,7 @@ def extractDigit(numblock,dtype):
     for c in cnts:
         extractedcount = extractedcount + 1
         (x, y, w, h) = cv2.boundingRect(c)
-        if h > hblock*0.85:
+        if h > hblock*dtype["dighighratio"]:
             digit = numblock[y:y + h, x:x + w]
             digitimg.append(digit)
 
@@ -125,33 +151,32 @@ def extractDigit(numblock,dtype):
     return digitimg
     
 def recognizeDigit(digit,dtype):
-
-    h,w = digit.shape
-    error = []
-    total = 0
+	temptype = dtype["temps"]
+	h,w = digit.shape
+	error = []
+	total = 0
     
-    for idx,current in enumerate(temptype1): 
-        temps = cv2.resize(temptype1[idx], (w,h) , interpolation = cv2.INTER_AREA)
-        difference = cv2.countNonZero( digit - temps )
-        total = total + difference
-        error.append(difference)
+	for idx,current in enumerate(temptype): 
+		temps = cv2.resize(temptype[idx], (w,h) , interpolation = cv2.INTER_AREA)
+		difference = cv2.countNonZero( digit - temps )
+		total = total + difference
+		error.append(difference)
         
-    average = total / 10
-    val, idx = min((val, idx) for (idx, val) in enumerate(error))
-    confidence = round((average - val)/average , 3)*100
+	average = total / 10
+	val, idx = min((val, idx) for (idx, val) in enumerate(error))
+	confidence = round((average - val)/average , 3)*100
     
-    if confidence < 30:
-        cv2.imshow("x", digit)
-        k = cv2.waitKey(0)
-        idx = chr(k)
-        confidence = 100
-    else:
-        idx = str(idx)
-        
-    return idx , confidence
+	if confidence < 30:
+		cv2.imshow("x", digit)
+		k = cv2.waitKey(0)
+		idx = chr(k)
+		confidence = 100
+	else:
+		idx = str(idx)
 
+	return idx , confidence
 
-def digits2string(digits,dchar,dman,dtype):
+def digits2string(digits,dchar,dtype):
     digitLst = []
     confidenceLst = []
     for digit in digits:
@@ -165,13 +190,13 @@ def digits2string(digits,dchar,dman,dtype):
     out = charlst + '.' + manlst
     return out
 
-im = cv2.imread("testing images/metertest8.png")
-device = findDevice(im,0)
-screen = findScreen(device,0)
-textblock = extractTextImg(screen,0)
-digits = extractDigit(textblock[0],0)
-#confidence , digit = recognizeDigit(digits[0] , 0)
-digitlist= digits2string(digits,3,0,0)
+
+im = cv2.imread("testing images/metertest9.png")
+device = findDevice(im,type1)
+screen = findScreen(device,type1)
+textblock = extractTextImg(screen,type1)
+digits = extractDigit(textblock[0],type1)
+digitlist= digits2string(digits,3,0,type1)
 print(digitlist)
 
 
